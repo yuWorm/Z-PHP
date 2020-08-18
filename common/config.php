1cd24c9a7339047deada3d5d743808f8ceba8599
playboy <278567383@qq.com>
2020-08-18 23:43:00 +0800

    调整配置文件字段

--- common/config.php
+++ common/config.php
@@ -1,35 +1,39 @@
 <?php
-  return [
-    'DEBUG' => ['level' => 3],
+return [
+    'DEBUG' => [
+        'level' => 3, // 输出全部debug信息
+        'type' => 'json', // 输出json格式的debug信息
+    ],
     'ROUTER' => [
-      'mod' => 0, //0:queryString，1：pathInfo，2：路由
-      'module' => false,
+        'mod' => 0, // 0:queryString，1：pathInfo，2：路由
+        'module' => false, // 不启用模块模式
+        'restfull' => 0,
     ],
-    'VER'=>['1.0', '1.0'],//[0]:默认版本号:没有请求版本号或找不到请求版本号对应目录的情况下使用此版本号,[1]:强制指定版本号：无视请求版本号，一律使用此版本号
-    'SESSION'=>[
-      'name'=>'TOKEN', //session 名
-      'auto'=>true, //自动开启 session
-      'redis'=>false,
-      'host'=>'',
-      'port'=>'',
-      'pass'=>''
+    'VER' => ['1.0', '1.0'], // [0]:默认版本号:没有请求版本号或找不到请求版本号对应目录的情况下使用此版本号,[1]:强制指定版本号：无视请求版本号，一律使用此版本号
+    'SESSION' => [
+        'name' => 'TOKEN', // session 名
+        'auto' => true, // 自动开启 session
+        'redis' => false,
+        'host' => '',
+        'port' => '',
+        'pass' => '',
     ],
-    'VIEW'=>[
-      'php_tag'=>'php',
-      'import_tag'=>'import',
-      'template_tag'=>'template',
-      'ext'=>'.html',
-      'theme'=>'default',
-      'prefix'=>'<{',
-      'suffix'=>'}>',
-      'compress'=>false
+    'VIEW' => [
+        'php_tag' => 'php',
+        'import_tag' => 'import',
+        'template_tag' => 'template',
+        'ext' => '.html',
+        'theme' => 'default',
+        'prefix' => '<{',
+        'suffix' => '}>',
+        'compress' => false,
     ],
-    'DB'=>[
-      'dsn'=>'mysql:host=127.0.0.1;dbname=zphp_test;port=3306',
-      'db'=>'zphp_test',
-      'user'=>'zphp_test',
-      'pass'=>'zphp_test',
-      'charset'=>'utf8',
-      'prefix'=>'z_'
+    'DB' => [
+        'dsn' => 'mysql:host=127.0.0.1;dbname=zphp_test;port=3306',
+        'db' => 'zphp_test',
+        'user' => 'zphp_test',
+        'pass' => 'zphp_test',
+        'charset' => 'utf8',
+        'prefix' => 'z_',
     ],
-  ];
+];
