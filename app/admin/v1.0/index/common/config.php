32ed1e2c7d375cd0dc46beb3769f38fa7bf47681
playboy <278567383@qq.com>
2020-08-18 23:36:52 +0800

    调整配部分置文件字段

--- app/admin/v1.0/index/common/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-return [
-    'DEBUG' => 2,
-    'MODULE' => false,
-    'URL_MOD' => 0, //0:queryString，1：pathInfo，2：路由
-];
--- app/demo/v1.0/common/config.php
+++ app/demo/v1.0/common/config.php
@@ -1,6 +1,12 @@
 <?php
 return [
-    'DEBUG' => 2,
-    'MODULE' => false,
-    'URL_MOD' => 2, //0:queryString，1：pathInfo，2：路由
+    'DEBUG' => [
+        'level' => 3, // 输出全部debug信息
+        'type' => 'json', // 输出json格式的debug信息
+    ],
+    'ROUTER' => [
+        'mod' => 2, // 0:queryString，1：pathInfo，2：路由
+        'module' => false, // 不启用模块模式
+        'restfull' => 0,
+    ],
 ];
